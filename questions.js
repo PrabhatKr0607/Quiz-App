6576ef6f697c3febc910e30265489f053aa7606d
Prabhat Kumar <86422560+PrabhatKr0607@users.noreply.github.com>
2021-10-04 15:35:53 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,122 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Web page editors works on which principle.",
+    answer: "WYSIWYG",
+    options: [
+      "WWW",
+      "HTML",
+      "WYSIWYG",
+      "WYGWYSI"
+    ]
+  },{
+    numb: 7,
+    question: "Which program is used by web clients to view the web pages?",
+    answer: "Web browser",
+    options: [
+      "Web browser",
+      "Protocol",
+      "Web Server",
+      "Search Engine"
+    ]
+  },{
+    numb: 8,
+    question: "What is the name of the location address of the hypertext documents?",
+    answer: "Uniform Resource Locator",
+    options: [
+      "Uniform Resource Locator",
+      "Web serve",
+      "File",
+      "Web Address"
+    ]
+  },{
+    numb: 9,
+    question: "What are shared on the Internet and are called as Web pages?",
+    answer: "Hypertext documents",
+    options: [
+      "Programs",
+      "Cables",
+      "Hypertext documents",
+      "None"
+    ]
+  },{
+    numb: 10,
+    question: "How many colour names are used by the browsers?",
+    answer: "16",
+    options: [
+      "8",
+      "10",
+      "12",
+      "16"
+    ]
+  },// you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9....g
+
+  //   {
+  //   numb: 11,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
